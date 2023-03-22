e75cdc0895d7995088e6d5674bead63bc0611be2
zenonsenn <zenonsenn@gmail.com>
2023-03-22 23:07:56 +0700

    e
    e

--- backend/midtrans.js
+++
@@ -1,7 +0,0 @@
-const midtransClient = require("midtrans-client");
-
-export let coreApi = new midtransClient.CoreApi({
-    isProduction: false,
-    serverKey: process.env.REACT_APP_MIDTRANS_SERVER_KEY,
-    clientKey: process.env.REACT_APP_MIDTRANS_CLIENT_KEY,
-});
