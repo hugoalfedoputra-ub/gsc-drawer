d0c965a8db990542fdcebbb0cf2d24ecdf83443e
zenonsenn <zenonsenn@gmail.com>
2023-03-31 10:23:23 +0700

    nn
    nn

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@ app.listen(5000 || process.env.PORT, () => {
 
 console.log("hello world");
 
-const serviceAccount = require((module = {
+const serviceAccount = {
     type: "service_account",
     project_id: "drawer-webapp",
     private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID.toString(),
@@ -31,7 +31,7 @@ const serviceAccount = require((module = {
     token_uri: "https://oauth2.googleapis.com/token",
     auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
     client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-hkovu%40drawer-webapp.iam.gserviceaccount.com",
-}));
+};
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
